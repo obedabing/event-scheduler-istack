49bfab3e765882635926c8aae7c7418d549766f7
obed abing <obed03abing@gmail.com>
2021-05-25 01:23:55 +0800

    created search container components

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,16 +1,150 @@
-import React from 'react'
-import Container from '@material-ui/core/Container'
+import React, { useState } from 'react'
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import { makeStyles } from '@material-ui/core/styles'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormLabel from '@material-ui/core/FormLabel'
+import FormControl from '@material-ui/core/FormControl'
+import FormGroup from '@material-ui/core/FormGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import FormHelperText from '@material-ui/core/FormHelperText'
+import Accordion from '@material-ui/core/Accordion'
+import AccordionSummary from '@material-ui/core/AccordionSummary'
+import AccordionDetails from '@material-ui/core/AccordionDetails'
 import Typography from '@material-ui/core/Typography'
-import Box from '@material-ui/core/Box'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import Button from '@material-ui/core/Button'
+import Link from '@material-ui/core/Link'
 
-export default function Index() {
+import {
+  topicIds,
+  topics,
+} from '../src/constants'
+
+const useStyles = makeStyles({
+  searchContainer: {
+    height: '90vh',
+    backgroundColor: '#F6F9FE', 
+    padding: '15px',
+  },
+  eventsContainer: {
+    height: '90vh',
+    padding: '15px',
+  },
+  showFilter: {
+    color: '#0A4AFA',
+    textDecoration: 'underline',
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+  filterCheckboxContainer: {
+    marginRight: '10px',
+    width: '150px'
+  },
+})
+
+const Index = () => {
+  const classes = useStyles()
+  const [showFilter, setShowFilter] = useState(false)
+
+  const handleShowFilter = () => {
+    setShowFilter(!showFilter)
+  }
+  
+  const renderSearchContainer = () => {
+
+
+    return (
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            required
+            placeholder="Search..."
+            variant="filled"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Grid
+            container
+            spacing={2}
+            justify="space-between"
+          >
+            <Grid item>
+              <Typography variant="h6">
+                Refined by
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Link onClick={handleShowFilter}>
+                <Typography className={classes.showFilter}>
+                  Show all filters
+                </Typography>
+              </Link>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Accordion
+            expanded={showFilter}
+            onClick={handleShowFilter}
+          >
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Grid
+                container
+                spacing={2}
+                justify="space-between"
+              >
+                <Grid item>
+                  <FormControlLabel
+                    control={<Checkbox
+                      name="track"
+                      onChange={() => {
+                        if (showFilter) {
+                          setShowFilter(true)
+                        } else {
+                          setShowFilter(false)
+                        }
+                      }}
+                    />}
+                    label="Track"
+                  />
+                </Grid>
+              </Grid>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Grid container spacing={2}>
+                {
+                  topicIds.map((id) => {
+                    const data = topics[id]
+                    return (
+                      <Grid item className={classes.filterCheckboxContainer}>
+                        <FormControlLabel
+                          control={<Checkbox name={data.name} />}
+                          label={data.name}
+                        />
+                      </Grid>
+                    )
+                  })
+                }
+              </Grid>
+            </AccordionDetails>
+          </Accordion>
+        </Grid>
+      </Grid>
+    )
+  }
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
-        </Typography>
-      </Box>
-    </Container>
+    <Grid container>
+      <Grid item xs={3} className={classes.searchContainer}>
+        {renderSearchContainer()}
+      </Grid>
+      <Grid item xs={9} className={classes.eventsContainer}>
+        Sidebar
+      </Grid>
+    </Grid>
   )
 }
+
+export default Index
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -15,3 +15,20 @@ export const cookieOptions = {
 }
 
 export { Cookies as cookieStorage }
+
+export const topics = {
+  'advertising_101': { name: 'Adevertising 101', type: 'advertising_101' },
+  'advertising_agencies': { name: 'Adevertising Agencies', type: 'advertising_agencies' },
+  'afilliate_marketing': { name: 'Afilliate Marketing', type: 'afilliate_marketing' },
+  'content_marketing': { name: 'Content Marketing', type: 'content_marketing' },
+  'coversion_optimization': { name: 'Conversion Optimizatoin', type: 'coversion_optimization' },
+  'design_for_growth': { name: 'Design for Growth', type: 'design_for_growth' },
+  'ecommerce_d2c': { name: 'Ecommerce/D2C', type: 'ecommerce_d2c' },
+  'influencer_marketing': { name: 'Influencer Marketing', type: 'influencer_marketing' },
+  'lead_generation': { name: 'Lead Generation', type: 'lead_generation' },
+  'media_buying': { name: 'Media Buying', type: 'media_buying' },
+  'seo_sem': { name: 'SEO & SEM', type: 'seo_sem' },
+  'future_advertising': { name: 'The Future of Advertising', type: 'future_advertising' },
+}
+
+export const topicIds = Object.keys(topics)
