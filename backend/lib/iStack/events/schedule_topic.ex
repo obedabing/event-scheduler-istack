5c91f27c17e37366d546be206ff86de064d16930
obed abing <obed03abing@gmail.com>
2021-05-26 23:09:48 +0800

    added validation to topics

--- backend/lib/iStack/events/schedule_topic.ex
+++ backend/lib/iStack/events/schedule_topic.ex
@@ -20,5 +20,20 @@ defmodule IStack.Events.ScheduleTopic do
     schedule_topic
     |> cast(attrs, [:title, :description, :stage, :track_type, :author_name, :author_title])
     |> validate_required([:title, :description, :stage, :track_type, :author_name, :author_title])
+    |> validate_inclusion(:stage, ["stage_one", "stage_two", "stage_three"])
+    |> validate_inclusion(:track_type, [
+      "advertising_101",
+      "advertising_agencies",
+      "afilliate_marketing",
+      "content_marketing",
+      "coversion_optimization",
+      "design_for_growth",
+      "ecommerce_d2c",
+      "influencer_marketing",
+      "lead_generation",
+      "media_buying",
+      "seo_sem",
+      "future_advertising",      
+    ])
   end
 end
