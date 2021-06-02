662e464717b6450ea367238b11037e1025162c40
obed abing <obed03abing@gmail.com>
2021-06-02 22:56:23 +0800

    fixed styles of topic modal info

--- frontend/src/components/TopicModalInfo/index.js
+++ frontend/src/components/TopicModalInfo/index.js
@@ -9,15 +9,40 @@ import IconButton from '@material-ui/core/IconButton'
 import Divider from '@material-ui/core/Divider'
 import CloseIcon from '@material-ui/icons/Close'
 import RoomIcon from '@material-ui/icons/Room'
+import { makeStyles } from '@material-ui/core/styles'
 
 import TrackLabel from '../TrackLabel'
 import { stages } from '../../constants'
+
+const useStyles = makeStyles({
+  dialogTitle: {
+    paddingBottom: '0px',
+  },
+  dialogContent: {
+    paddingBottom: '20px',
+  },
+  roomIcon: {
+    fontSize: '14px',
+    margin: '5px',
+    color: '#F43C51',
+  },
+  title: {
+    fontWeight: 900,
+  },
+  authorName: {
+    fontSize: '18px',
+  },
+  acountBoxIcon: {
+    fontSize: '60px'
+  }
+})
 
 const TopicModalInfo = ({
   data = {},
   openModal = false,
   onClose = () => {},
 }) => {
+  const classes = useStyles()
   const handleCloseModal = () => {
     onClose()
   }
@@ -29,7 +54,7 @@ const TopicModalInfo = ({
       maxWidth="xs"
       fullWidth
     >
-      <DialogTitle style={{ paddingBottom: '0px' }}>
+      <DialogTitle className={classes.dialogTitle}>
         <Grid container>
           <Grid
             xs={10}
@@ -39,9 +64,7 @@ const TopicModalInfo = ({
           >
             <TrackLabel
               type={data.trackType}
-              style={{
-                fontSize: '15px'
-              }}
+              style={{ fontSize: '15px' }}
             />
           </Grid>
           <Grid
@@ -58,19 +81,10 @@ const TopicModalInfo = ({
           </Grid>
         </Grid>
       </DialogTitle>
-      <DialogContent
-        style={{
-          paddingBottom: '20px',
-        }}
-      >
+      <DialogContent className={classes.dialogContent}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <Typography
-              variant="h6"
-              style={{
-                fontWeight: 900,
-              }}
-            >
+            <Typography variant="h6" className={classes.title}>
               {data.title}
             </Typography>
           </Grid>
@@ -81,13 +95,7 @@ const TopicModalInfo = ({
             alignItems="center"
             xs={12}
           >
-            <RoomIcon
-              style={{
-                fontSize: '14px',
-                margin: '5px',
-                color: '#F43C51',
-              }}
-            />
+            <RoomIcon className={classes.roomIcon}/>
             <Typography variant="body2">
               {stages[data.stage].name}
             </Typography>
@@ -111,7 +119,7 @@ const TopicModalInfo = ({
               xs={9}
               direction="column"
             >
-              <Typography variant="subtitle2" style={{ fontSize: '18px' }}>
+              <Typography variant="subtitle2" className={classes.authorName}>
                 {data.authorName}
               </Typography>
               <Typography variant="body2">
@@ -119,9 +127,7 @@ const TopicModalInfo = ({
               </Typography>
             </Grid>
             <Grid item container justify="flex-end" xs={3}>
-              <AccountBoxIcon
-                style={{ fontSize: '60px' }}
-              />
+              <AccountBoxIcon className={classes.acountBoxIcon}/>
             </Grid>
           </Grid>
         </Grid>
